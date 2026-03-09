fb29f6b6c77e54e21e4aba4fd50a50d497855671
mgf <mgf@ugr.es>
2026-03-09 13:09:35 +0100

    style: ajustar encuadre de la portada de Horizonte de Sucesos

--- constants.ts
+++ constants.ts
@@ -39,7 +39,7 @@ export const BOOKS: Book[] = [
     year: '2026',
     description: `Tras una larga pausa para centrarse en su carrera profesional, Belén ha retomado la poesía con su nuevo libro "Horizonte de Sucesos".`,
     coverUrl: '/images/libros/Horizonte _de_Sucesos.JPG',
-    coverPosition: 'object-center',
+    coverPosition: 'object-cover object-[center_30%]',
     purchaseUrl: '#'
   }
 ];
